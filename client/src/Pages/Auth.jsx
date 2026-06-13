25c682e20198ca10e3b6b1dfaacba5cb6550573f
Naveen4222 <naveen7082796682@gmail.com>
2026-06-13 18:21:37 +0530

    Home page styling

--- client/src/Pages/Auth.jsx
+++ client/src/Pages/Auth.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import {motion} from "motion/react";
 
 const Auth = () => {
   return (
-    <div>
-      Auth PagesS
+    <>
+    <div className="min-h-screen overflow-hidden bg-white text-black px-8">
+        <motion.header
+        initial ={{opacity:0 , y:-15}}
+        animate={{opacity:1, y:0}}
+        transition={{duration:1.5 }}
+        className="max-w-7xl mx-auto mt-8 
+        rounded-2xl bg-black/80 backdrop-blur-xl
+        border border-white/10
+        px-8 py-6
+        shadow-[0_20px_45px_rgba(0,0,0,0.6]">
+
+            <h1 className='text-2xl font-bold
+            bg-gradient-to-r from-white via-gray-300 to-white
+            bg-clip-text text-transparent '>ExamNotes Ai</h1>
+            <p className='text-sm text-gray-300 mt-1'>
+                AI-powered exam-oriented notes & revision
+            </p>
+        </motion.header>
     </div>
+    </>
   )
 }
 
