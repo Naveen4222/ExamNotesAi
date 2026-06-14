f495ee976cf23a5b7b421780178581351f7ebaf9
Naveen4222 <naveen7082796682@gmail.com>
2026-06-14 15:03:08 +0530

    minor change

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -2,10 +2,16 @@ import userModel from "../models/user_model.js";
 
 export const getCurrentUser = async(req,res )=>{
     try {
+        
+        
         const userId = req.userId;
+        console.log("req.userId", req.userId);
+        console.log("Searching for:", userId);
+
         const user = await userModel.findById(userId);
+        console.log("User found", user);
         if(!user){
-            return resizeBy.status(400).json({msg:"User is not found"});
+            return res.status(400).json({msg:"User is not found"});
 
         }
         return res.status(200).json(user);
--- server/middlerware/isAuth.js
+++ server/middlerware/isAuth.js
@@ -2,20 +2,33 @@ import jwt from "jsonwebtoken";
 
 const isAuth = async (req, res, next) => {
     try {
-        let { token } = req.cookies;
+        console.log("Cookies:", req.cookies);
+
+        const { token } = req.cookies || {};
+
         if (!token) {
-            return res.status(400).json({ msg: "Token is not found" })
+            return res.status(401).json({
+                msg: "Token not found",
+            });
         }
-        let verifyToken = jwt.verify(token, process.env.JWT_SECRET);
-        if (!verifyToken) {
-            res.status(400).json({ msg: "user doesn't have valid token" })
-        }
+
+        const verifyToken = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+        console.log(verifyToken);
+
         req.userId = verifyToken.userId;
+
         next();
 
     } catch (error) {
-        return res.status(500).json({message:`is auth error ${error}`})
+        console.error("isAuth error:", error);
+
+        return res.status(500).json({
+            message: error.message,
+        });
     }
-}
+};
 
 export default isAuth;
